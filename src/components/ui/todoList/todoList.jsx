dc3850a20512d2e1c2ad83f38beabf8ba26cf08d
VladT8 <tishuk.vladzislau@gmail.com>
2024-02-04 22:39:41 +0300

    add todo-list + styles

--- src/components/ui/todoList/todoList.jsx
+++ src/components/ui/todoList/todoList.jsx
@@ -2,5 +2,11 @@ import { TodoItem } from "../todoItem/todoItem";
 import "./todoList.scss";
 
 export const TodoList = ({ todos }) => {
-  return todos.map((todo, index) => <TodoItem key={index} todo={todo} />);
+  return (
+    <div className="todo-list">
+      {todos.map((todo, index) => (
+        <TodoItem key={index} todo={todo} />
+      ))}
+    </div>
+  );
 };

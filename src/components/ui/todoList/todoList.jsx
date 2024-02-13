371aa94ce23956317ccd0fd8a8f984e418ed26ef
VladT8 <tishuk.vladzislau@gmail.com>
2024-02-13 17:24:50 +0300

    add text for empty list

--- src/components/ui/todoList/todoList.jsx
+++ src/components/ui/todoList/todoList.jsx
@@ -4,6 +4,9 @@ import "./todoList.scss";
 export const TodoList = ({ todos }) => {
   return (
     <div className="todo-list">
+      {!todos.length && (
+        <h2 className="todo-list__empty">Your todo list is empty</h2>
+      )}
       {todos.map((todo, index) => (
         <TodoItem key={index} todo={todo} />
       ))}
